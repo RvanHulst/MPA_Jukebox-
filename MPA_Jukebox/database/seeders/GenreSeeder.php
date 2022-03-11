a375a2b71695e6810d99beaa05cf06592bd34a0d
RvanHulst <99051006@mydavinci.nl>
2022-03-11 14:19:07 +0100

    Images on Genre added

--- MPA_Jukebox/database/seeders/GenreSeeder.php
+++ MPA_Jukebox/database/seeders/GenreSeeder.php
@@ -16,16 +16,23 @@ class GenreSeeder extends Seeder
     {
         DB::table('genres')->insert([
             [
-            'genre' => 'Metal'
+            'genre' => 'Metal',
+            'image' => 'https://flypaper.soundfly.com/wp-content/uploads/2016/10/metal-covers-header.png',
             ],[
-            'genre' => 'Jazz'
+            'genre' => 'Jazz',
+            'image' => 'https://static.nieuwsblad.be/Assets/Images_Upload/2016/03/07/michaelasimos.jpg',
             ],[
-            'genre' => 'Blues'
+            'genre' => 'Blues',
+            'image' => 'https://st.depositphotos.com/1269378/2059/v/600/depositphotos_20592453-stock-illustration-blues-musician.jpg',
             ],[
-            'genre' => 'Rock'
+            'genre' => 'Rock',
+            'image' => 'https://sm.ign.com/t/ign_nl/news/i/is-harmonix-creating-a-new-rock-band-game/is-harmonix-creating-a-new-rock-band-game_wfv8.h720.jpg',
             ],[
-            'genre' => 'Hiphop'
+            'genre' => 'Hiphop',
+            'image' => 'https://www.dansisvooriedereen.nl/wp-content/uploads/2020/06/461d2d6b331f3999ddae6304c1276d74.jpg'
             ]
         ]);
+
+        
     }
 }
--- MPA_Jukebox/resources/views/genre.blade.php
+++ MPA_Jukebox/resources/views/genre.blade.php
@@ -6,10 +6,9 @@
         <div class="container-fluid d-flex flex-wrap justify-content-evenly">
         @foreach ($genres as $genre)
         <div class="card" style="width: 18rem;">
-            <img src="https://picsum.photos/1920/1080" class="card-img-top">
+            <img src={{ $genre->image }} class="card-img-top">
             <div class="card-body">
                 <h5 class="card-title">{{ $genre->genre }}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Go to {{ $genre->genre }}</a>
             </div>
         </div>
