6f0b8c2b947d5cb405fcd332f2dcb1bb9692f3e2
RvanHulst <99051006@mydavinci.nl>
2022-03-25 12:10:04 +0100

    New song info layout

--- MPA_Jukebox/resources/views/song.blade.php
+++ MPA_Jukebox/resources/views/song.blade.php
@@ -1,20 +1,27 @@
 @include('includes.header')
 @include('includes.navbar')
-    <div class="container-fluid d-flex flex-wrap justify-content-evenly">
-      <div class="card" style="width: 20rem;">
-        <img src={{ $song->image }} class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title"> <a href="{{ url('/song', $song->id) }}">{{ $song->name }} </a></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Artist/Band: {{ $song->artistband }}</li>
-              <li class="list-group-item">Song duration: {{ $song->duration }}</li>
-              <li class="list-group-item">Song Genre: {{ $song->genre->genre }}</li>
-            </ul>
-            <div class="card-body">
-            <a href="#" class="card-link btn btn-success">Add song</a>
-            </div>
-      </div>
+<!-- Page Content -->
+<div class="container">
+  <!-- Portfolio Item Row -->
+  <div class="row">
+
+    <div class="col-md-8">
+     <img src={{ $song->image }} class="card-img-top">
     </div>
+
+    <div class="col-md-4">
+      <h3 class="my-3">{{ $song->name }}</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+      <h3 class="my-3">Song Details</h3>
+      <ul>
+        <li class="list-group-item">Artist/Band: {{ $song->artistband }}</li>
+        <li class="list-group-item">Song duration: {{ $song->duration }}</li>
+        <li class="list-group-item">Song Genre: {{ $song->genre->genre }}</li>
+        <li class="list-group-item"><a href="#" class="card-link btn btn-success">Add song</a></li>
+      </ul>
+    </div>
+  </div>
+  <!-- /.row -->
+</div>
+<!-- /.container -->
 @include('includes.footer')
